9bd51e1f9a65fafb26746f163ec503ba9eedeae3
Danilka312 <112857353+Danilka312@users.noreply.github.com>
2023-04-13 23:28:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,11 @@
-import pandas as pd
-import numpy as np
+import pandas as pdimport numpy as np
+from statsmodels.stats.proportion import proportions_ztestfrom scipy. special import logsumexp
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+chat_id = 6122152745
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
+    tmp = ((x_success+y_success) * y_cnt)/(x_cnt+y_cnt)    
+    sqr = (y_success - tmp)**2/tmp
+    if sqr < 6.63:        
+      return False
+    else:        
+      return True
